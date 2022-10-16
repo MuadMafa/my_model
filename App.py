529d980b0f04418aaf30f0cbf7883e23abc30c2f
MuadMafa <109862210+MuadMafa@users.noreply.github.com>
2022-10-16 15:51:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -28,10 +28,10 @@ def import_and_predict(image_data, model):
 
 if file==None:
     st.text("Please Upload a file")
-    else:
-        image = Image.open(file)
-        st.image(image, use_column_width=True)
-        predictions = import_and_predict(image, model)
-        class_names = ['daisy', 'dandelion', 'roses' , 'sunflowers', 'tulips']
-        string = "The Flower in the Image is most likely is : " + class_names[np.argmax(predictions) ]
-        st.success(string)
+ else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    class_names = ['daisy', 'dandelion', 'roses' , 'sunflowers', 'tulips']
+    string = "The Flower in the Image is most likely is : " + class_names[np.argmax(predictions) ]
+    st.success(string)
